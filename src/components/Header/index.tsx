c2dd505e9c636f8c01dc391e225edeb535a8a7de
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-09 15:15:16 -0300

    feat(header buttons): Home and Posts buttons are now stylized

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
 
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss'
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <Image src="/images/logo.svg" alt="ig.news" width="110" height="31" />
                 <nav>
-                    <Link href="/">
-                        <a className={styles.active}>Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
                         <a>Posts</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
