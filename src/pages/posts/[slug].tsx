e1b773dbc0dd7bab99b5bc189f95e203d73ccbec
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-10 14:38:32 -0300

    fix(posts): posts button redirects unsubribed user to post preview

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,14 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const session = await getSession({ req })
     const { slug } = params;
 
-    if (!session?.activeSubscription) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            }
-        }
-    }
+
 
     const prismic = getPrismicClient(req)
 
@@ -63,6 +56,15 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
             month: 'long',
             year: 'numeric'
         })
+    }
+
+    if (!session?.activeSubscription) {
+        return {
+            redirect: {
+                destination: `/posts/preview/${post.slug}`,
+                permanent: false,
+            }
+        }
     }
 
     return {
